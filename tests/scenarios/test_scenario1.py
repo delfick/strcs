c54b8a1939e473beba4ad7fc85dfd3ac87ad0291
Stephen Moore <stephen@delfick.com>
2023-01-02 18:03:38 +1100

    generics: add tests

--- tests/scenarios/test_scenario1.py
+++ tests/scenarios/test_scenario1.py
@@ -10,6 +10,8 @@ import strcs
 reg = strcs.CreateRegister()
 creator = reg.make_decorator()
 
+T = tp.TypeVar("T")
+
 
 @define
 class Project:
@@ -24,8 +26,63 @@ class Detail:
 
 
 @define
+class Item:
+    one: int
+    two: int
+
+
+@define
+class ItemOne(Item):
+    one: int = 20
+    two: int = 50
+    three: bool = False
+
+
+@define
+class ItemTwo(Item):
+    one: int = 3
+    two: int = 5
+    four: bool = True
+
+
+@define
+class Container(tp.Generic[T]):
+    category: str
+    item: T
+
+
+@define
 class Projects:
     projects: tp.List[Project] = field(factory=lambda: [])
+
+
+@creator(Item)
+def create_item(item: object) -> dict | None:
+    if item is strcs.NotSpecified:
+        item = {}
+    if isinstance(item, dict):
+        return item
+    return None
+
+
+categories: dict[str, type[Item]] = {"one": ItemOne, "two": ItemTwo}
+
+
+@creator(Container)
+def create_container(
+    item: object, want: strcs.Type[Container], /, _register: strcs.CreateRegister
+) -> dict | None:
+    if not isinstance(item, dict):
+        return None
+
+    (expected,) = want.find_generic_subtype(Item)
+    if "category" not in item:
+        return None
+    using = categories.get(item["category"])
+    assert using is expected
+
+    item["item"] = _register.create(using, item.get("item", strcs.NotSpecified))
+    return item
 
 
 @creator(Projects)
@@ -101,3 +158,30 @@ describe "Making the projects":
 
         assert projects.projects[1].details[0].project is project2
         assert projects.projects[1].details[1].project is project2
+
+    it "can make generic types":
+        container = reg.create(Container[ItemOne], {"category": "one"})
+        assert isinstance(container.item, ItemOne)
+        assert container.item.one == 20
+        assert container.item.two == 50
+        assert not container.item.three
+
+        container2 = reg.create(Container[ItemTwo], {"category": "two"})
+        assert isinstance(container2.item, ItemTwo)
+        assert container2.item.one == 3
+        assert container2.item.two == 5
+        assert container2.item.four
+
+    it "can complain about asking for the wrong subtype":
+        with pytest.raises(strcs.errors.UnableToConvert) as e:
+            reg.create(Container[ItemOne], {"category": "two"})
+
+        assert str(e.value.error) == (
+            "assert <class 'tests.scenarios.test_scenario1.ItemTwo'> is <class 'tests.scenarios.test_scenario1.ItemOne'>"
+        )
+
+    it "can complain about not asking for a subtype":
+        with pytest.raises(strcs.errors.UnableToConvert) as e:
+            reg.create(Container)
+
+        assert e.value.reason == "Converter didn't return a value to use"
