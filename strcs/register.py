8a416d84a1483fd5a86a367b5d9ad42cc0efbdbe
Stephen Moore <stephen@delfick.com>
2023-10-15 09:40:23 +1100

    register: Adding missing type annotation

--- strcs/register.py
+++ strcs/register.py
@@ -159,7 +159,7 @@ class CreateRegister:
 
             register: tp.ClassVar[CreateRegister]
 
-            def __init__(self, typ: object, *, assume_unchanged_converted=True):
+            def __init__(self, typ: object, *, assume_unchanged_converted: bool = True):
                 self.original = typ
                 self.assume_unchanged_converted = assume_unchanged_converted
 
